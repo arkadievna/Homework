823d53850ab184df3ec07cbd44b3827747294ad0
arcadievna <reshetnikovaya@mail.ru>
2023-09-14 21:46:59 +0500

    добавила ДЗ 2

---
+++ Homework-2/Задача 13-2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите целое положительное число:");
+            string? number = Console.ReadLine()!;
+            Console.WriteLine("Какой по счёту ищем элемент?");
+            string? position = Console.ReadLine()!;
+            if (int.TryParse(number, out var numInt) && int.TryParse(position, out var posInt))
+            {
+                findPosition(numInt, posInt);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод!");
+            }
+            Console.WriteLine("Нажмите любую клавишу или введите 'q' и нажмите Enter для выхода.");
+            if (Console.ReadLine() == "q") break;
+        }
+
+        static void findPosition(int num, int pos)
+        {
+            if (Math.Log10(num) < pos - 1)
+            {
+                Console.WriteLine($"Нет цифры в позиции {pos}!");
+            }
+            else
+            {
+                while (num > 0)
+                {
+                    if (Math.Log10(num) > pos - 1 && Math.Log10(num) < pos) { Console.WriteLine($"Цифра в позиции {pos} - {num % 10}."); }
+                    num /= 10;
+                }
+            }
+        }
+    }
+}
+
---
+++ Homework-2/Задача 15_решение GB/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// В решении просят ввести день недели, а не номер дня недели. 
+// Когда меня просят ввести/назвать день недели, 
+// я говорю название дня, а не порядковый номер.
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+
+int weekDay = Promt("Введите день недели");
+if (ValidateWeekday(weekDay))
+{
+    if (IsWeekend(weekDay))
+{
+    Console.WriteLine("Наконец-то выходной!");
+}
+else
+{
+    Console.WriteLine("Придётся работать!");
+}
+
+}
